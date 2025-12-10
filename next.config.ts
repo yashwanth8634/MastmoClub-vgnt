4dc19418ed82aad728313acc1597dcb39de6c541
yashwanth8634 <yashwanthreddykoppula@gmail.com>
2025-12-10 13:47:39 +0530

    Remove Sentry dependency for stable deployment - can be re-added after v1.0 launch

--- next.config.ts
+++ next.config.ts
@@ -1,33 +1,8 @@
 import type { NextConfig } from "next";
-import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {
   /* config options here */
   turbopack: {},
 };
 
-export default withSentryConfig(
-  nextConfig,
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-    // Suppresses source map uploading logs during build
-    silent: true,
-    org: "your-sentry-org",
-    project: "your-sentry-project",
-
-    // For all available options for Sentry SDK, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-
-    // Enables automatic instrumentation of Vercel Cron Monitors.
-    // See the following for more information:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/vercel-cron-monitors/
-    automaticVercelMonitors: true,
-  }
-);
+export default nextConfig;
